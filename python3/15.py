84f2e6bc0d01e0050e996022ec66c1435765ccbc
Floozutter <me@floozutter.site>
2024-10-04 04:40:24 -0700

    build up from two sum instead

--- python3/15.py
+++ python3/15.py
@@ -1,27 +1,22 @@
-from collections import Counter, defaultdict
+from collections import Counter
 
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        # filter out excessive duplicates from data
+        # filter out excessive duplicates and order
         counts = Counter(nums)
         for n in counts:
             counts[n] = min(counts[n], 3 if n == 0 else 2)
-        filtered = tuple(sorted(counts.elements()))
-        if len(filtered) < 3:
+        data = tuple(sorted(counts.elements()))
+        if len(data) < 3:
             return []
-        # find min and max of data to cut search
-        lower_a, lower_b = filtered[0], filtered[1]
-        upper_a, upper_b = filtered[-1], filtered[-2]
         # build set of triplets
         triplets: set[tuple[int, int, int]] = set()
-        subtotals: dict[int, set[tuple[int, int]]] = defaultdict(set)
-        for i, x in enumerate(filtered):
-            if -x < lower_a + lower_b:
-                break
-            if -x in subtotals:
-                for z, y in subtotals[-x]:
-                    a, b, c = sorted((z, y, x))
+        for i, x in enumerate(data):
+            visited: set[int] = set()
+            for j, y in filter(lambda pair: pair[0] != i, enumerate(data)):
+                difference = -x - y
+                if difference in visited:
+                    a, b, c = sorted((x, y, difference))
                     triplets.add((a, b, c))
-            for y in filtered[0:i]:
-                subtotals[y + x].add((y, x))
+                visited.add(y)
         return list(map(list, triplets))
